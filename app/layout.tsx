1daa2d494cc7a106d377baf7d8ca60bc0096a49b
balldev1 <132042116+balldev1@users.noreply.github.com>
2023-06-18 10:50:24 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default async function RootLayout({
         <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
-            <ModalProvider products={products} />
+            <ModalProvider products={[]} />
             <Sidebar songs={userSongs}>
               {children}
             </Sidebar>
